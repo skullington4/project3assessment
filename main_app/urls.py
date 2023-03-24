6cfb32b94e9d96b8a9255ce25cc4ea70eb6f7fbb
skullington4 <ksully121888@gmail.com>
2023-03-24 15:13:31 -0400

    Completed project

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.ItemCreate.as_view(), name='add'),
+    path('item/,<int:pk>/delete/', views.ItemDelete.as_view(), name='item_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Item
 
 def home(request):
-  return render(request, 'home.html')
+  wish_list = Item.objects.all()
+  return render(request, 'home.html', {
+     'wish_list':wish_list
+  })
+
+def add(request):
+    return render(request, 'add.html')
+
+
+class ItemCreate(CreateView):
+  model = Item
+  fields = ['description']
+
+
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/'
